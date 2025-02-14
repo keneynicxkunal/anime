526d707d0bdf56c12d1d028e8ac0fa2a3c1939cb
kunal <150831705+keneynicxkunal@users.noreply.github.com>
2025-02-14 11:21:09 +0530

    Update blogdetails.jsx

--- pages/blogdetails.jsx
+++ pages/blogdetails.jsx
@@ -3,8 +3,6 @@ import React from 'react'
 function BlogDetails() {
   return (
     <>
-  {/* Page Preloder */}
-  {/* <div id="preloder">
     <div className="loader" />
   </div> */}
   {/* Header Section Begin */}
