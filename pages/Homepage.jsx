65977fdc09582728607868e44df80b600618a996
kunal <150831705+keneynicxkunal@users.noreply.github.com>
2025-02-19 21:46:38 +0530

    Update Homepage.jsx

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useState } from 'react';
-// import { useEffect } from 'react';
 import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import OwlCarousel from 'react-owl-carousel';
